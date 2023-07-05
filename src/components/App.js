965510cba362b32ffbe4f9baa1774d7bdcbc0566
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-07-05 13:14:00 +0000

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App = ()=> {
+    let [text , setText] = useState("");
+    return (
+        <div>
+            <div className="parent">
+                <h1>Parent Component</h1>
+                <p>{text}</p>
+            <div className="child">
+                <h2>Child Component</h2>
+                <input type="text" onChange={(event)=>setText(event.target.value)}/>
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
